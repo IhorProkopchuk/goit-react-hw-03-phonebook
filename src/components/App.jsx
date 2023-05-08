36e4122956ceb6637d2c0c0dcab8547636bb2587
IhorProkopchuk <rectoon001@gmail.com>
2023-05-08 20:37:48 +0300

    Added check by number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,47 +6,42 @@ import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 
 class App extends Component {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-
   state = {
-  contacts: [],
-  filter: '',
+    contacts: [],
+    filter: '',
   };
-  
 
-  componentDidMount() { 
+  componentDidMount() {
     const contactsFormLocalStorage = localStorage.getItem('contacts');
-    if (contactsFormLocalStorage) { 
+    if (contactsFormLocalStorage) {
       this.setState({ contacts: JSON.parse(contactsFormLocalStorage) });
     }
   }
 
-  componentDidUpdate(prevProps, prevState) { 
-    if (this.state.contacts !== prevState.contacts) { 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
 
-
-
-
   addContact = newContact => {
-const existingContact = this.state.contacts.find(
-  contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-);
+    const existingContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
 
-if (existingContact) {
-  alert(`${newContact.name} is already in contacts.`);
-  return;
-}
+    if (existingContact) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+
+    const existingNumber = this.state.contacts.find(
+      contact => contact.number === newContact.number
+    );
+
+    if (existingNumber) {
+      alert(`${newContact.number} is already in contacts.`);
+      return;
+    }
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
@@ -79,9 +74,7 @@ if (existingContact) {
     return (
       <div className={styles.container}>
         <h1 className={styles.title}>Phonebook</h1>
-        <ContactForm
-          addContact={this.addContact}
-        />
+        <ContactForm addContact={this.addContact} />
         <h2 className={styles.title}>Contacts</h2>
         <Filter value={filter} onChange={this.handleChangeFilter} />
         <ContactList
